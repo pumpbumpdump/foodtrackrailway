8f7ca8ffc3081f608da76b2791e64f7ffd006729
Hans <hanzaristo@gmail.com>
2024-01-29 01:28:34 +0700

    sixth a

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ from decouple import config
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='your_default_secret_key')
 
 DEBUG = config('DEBUG')
 
