d279b505dd6cb15f987a6488dff59cdc95cd1d4f
Hans <hanzaristo@gmail.com>
2024-01-29 03:35:08 +0700

    seventeenth commit

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-8b27c.up.railway.app/register']
+CSRF_TRUSTED_ORIGINS = ['*']
 
 # Application definition
 
