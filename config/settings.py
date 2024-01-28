1c5dea2b7b9f2926cadb947d65cbeb7a91461698
Hans <hanzaristo@gmail.com>
2024-01-29 01:54:12 +0700

    ninth commit

--- config/settings.py
+++ config/settings.py
@@ -62,8 +62,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASS'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': '',
     }
 }
 
